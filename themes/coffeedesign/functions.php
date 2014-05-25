a3e72b271461ef59315ee5cfd130f11f312bb531
Rob LaFeve <rob@cremalab.com>
2014-05-24 22:29:33 -0500

    Addition of Event post type

--- themes/coffeedesign/functions.php
+++ themes/coffeedesign/functions.php
@@ -10,4 +10,40 @@ function cc_mime_types( $mimes ){
 }
 add_filter( 'upload_mimes', 'cc_mime_types' );
 
+// EVENT POST TYPE
+add_action('init', 'cptui_register_my_cpt_event');
+function cptui_register_my_cpt_event() {
+register_post_type('event', array(
+'label' => 'Events',
+'description' => '',
+'public' => true,
+'show_ui' => true,
+'show_in_menu' => true,
+'capability_type' => 'post',
+'map_meta_cap' => true,
+'hierarchical' => false,
+'rewrite' => array('slug' => 'events', 'with_front' => 1),
+'query_var' => true,
+'has_archive' => true,
+'menu_position' => '5',
+'supports' => array('title','editor','excerpt','comments','thumbnail'),
+'taxonomies' => array('speakers'),
+'labels' => array (
+  'name' => 'Events',
+  'singular_name' => 'Event',
+  'menu_name' => 'Events',
+  'add_new' => 'Add Event',
+  'add_new_item' => 'Add New Event',
+  'edit' => 'Edit',
+  'edit_item' => 'Edit Event',
+  'new_item' => 'New Event',
+  'view' => 'View Event',
+  'view_item' => 'View Event',
+  'search_items' => 'Search Events',
+  'not_found' => 'No Events Found',
+  'not_found_in_trash' => 'No Events Found in Trash',
+  'parent' => 'Parent Event',
+)
+) ); }
+
 ?>
