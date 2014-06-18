89cacc66e6dc0a823f9c5b580f3c1bb501636099
Rob LaFeve <rob@cremalab.com>
2014-06-18 00:50:51 -0500

    Mobile Nav Fixes #25 and includes: - Addition of toggle script to functions

--- themes/coffeedesign/js/functions.js
+++ themes/coffeedesign/js/functions.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function($) {
 
+
+  // Nav Toggle
+  $("a#nav-toggle").click(function() {
+    $(".navigation, a#nav-toggle").toggleClass("open");
+  });
+
+  $(".navigation a, a.layout-header-logo").click(function() {
+    if ($("a#nav-toggle").hasClass("open")) {
+      $(".navigation, a#nav-toggle").toggleClass("open");
+    }
+  });
+
+
   // Fitvids
   $(".event-content-info").fitVids();
 
