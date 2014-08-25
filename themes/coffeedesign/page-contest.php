4088bb51063fdbc3584e34c4dcfd8ec5db55de6e
Rob LaFeve <rob@cremalab.com>
2014-08-24 23:54:59 -0500

    Changed dates

--- themes/coffeedesign/page-contest.php
+++ themes/coffeedesign/page-contest.php
@@ -9,7 +9,7 @@
           <p>In preparation for our upcoming letterpress and screen printing event [link] we wanted to continue to shake things up a bit and put a call out to all of of you awesome Kansas City designers. We're running a three week contest, open to all local designers, to show us an amazing Coffee & Design mark. You have complete creative freedom, it doesn't have to match any brand, it simply must at least say "Coffee & Design." The top two designs will get made into screens and printed on shirts at our September event. So let your creative juices flow and show us what you got!</p>
           <p><em>Contest Details</em></p>
           <ul class="nest-5">
-            <li>Open for Submissions: August 20th - September 10th</li>
+            <li>Open for Submissions: August 25th - September 10th</li>
             <li>Voting: September 15th - September 21st</li>
             <li>Winners Announced: September 24th</li>
             <li>Live Printing: September 26th</li>
