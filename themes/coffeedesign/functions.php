f6b36d36a131147f3c16ee21b57cb32fcf11881b
Rob LaFeve <rob@cremalab.com>
2014-12-22 15:28:06 -0600

    Adds better excerpt and link style for post listings

--- themes/coffeedesign/functions.php
+++ themes/coffeedesign/functions.php
@@ -404,4 +404,9 @@ $name = $author;
 return $name;
 }
 
+function new_excerpt_more( $more ) {
+  return '&hellip; <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
+
 ?>
