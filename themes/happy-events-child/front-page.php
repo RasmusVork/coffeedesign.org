dbc60a57c10b7677fbf28e8c61ec1dbf81f74c80
Joe barber <joemarxbarber@gmail.com>
2017-06-04 21:49:23 -0500

    Styling Update

--- themes/happy-events-child/front-page.php
+++ themes/happy-events-child/front-page.php
@@ -20,7 +20,7 @@
             <div class="note">
         <div class="note-txt">
           <p>We're working hard on a new site, but in the meantime you can check out our latest event. </p>
-          <a class="survey" href="https://cd_callahancreek.eventbrite.com">Design on the Rocks w/ Callahan Creek</a>
+          <a class="survey" href="https://cd_callahancreek.eventbrite.com">DOTR w/ Callahan Creek</a>
         </div>
       </div>
       <div class="connect">
