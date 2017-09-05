0c93717c8d7e3f3ea005fc5d16a18b061c32609f
Joe barber <joemarxbarber@gmail.com>
2017-09-05 10:44:05 -0500

    update event

--- themes/happy-events-child/front-page.php
+++ themes/happy-events-child/front-page.php
@@ -20,7 +20,7 @@
             <div class="note">
         <div class="note-txt">
           <p>We're working hard on a new site, but in the meantime you can check out our latest event. </p>
-          <a class="survey" href="https://dotr_populous.eventbrite.com/?aff=website">Design on the Rocks w/ Populous</a>
+          <a class="survey" href="https://www.eventbrite.com/e/cd-behind-the-can-tickets-37368470100?aff=website">C&amp;D: Behind the Can</a>
         </div>
       </div>
       <div class="connect">
